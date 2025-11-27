5fab657b845e52122f51f7fc4fd952eb7c1943fd
Asher Salama <asher.salamaruah@post.runi.ac.il>
2025-11-27 23:48:44 +0200

    finished everything

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,6 +21,20 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+        for (int i = 0; i < sentences.length; i++) {
+            String sentence = sentences[i].toLowerCase();
+            
+            int j = 0; 
+            while (j < keywords.length){
+                String keyword = keywords[j].toLowerCase();
+                if (sentence.contains(keyword)){
+                    System.out.println(sentences[i]);
+                    break;
+                } 
+                j++;
+            } 
+            
+        }
+        
     }
 }
